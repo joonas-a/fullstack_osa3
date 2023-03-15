bda845202a82a87f77276e449ee941f8cf300e57
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-03-15 17:38:37 +0200

    fix regex

--- models/person.js
+++ models/person.js
@@ -26,7 +26,7 @@ const personSchema = new mongoose.Schema({
     required: true,
     validate: {
       validator: function (v) {
-        return /\d{2,3}-\d{5,6}/.test(v)
+        return /^\d{2,3}-\d{5,6}/.test(v)
       },
       message: (props) => `${props.value} is not a valid phone number!`,
     },
