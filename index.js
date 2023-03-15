fe1786d50042b386d6cf6a57758259a4d9fe5d06
Joonas Ahovalli <9369455+joonas-a@users.noreply.github.com>
2023-03-15 09:21:33 +0200

    9, 10, 11

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
 const express = require("express")
 const morgan = require("morgan")
+const cors = require("cors")
 const app = express()
 
 app.use(express.json())
 morgan.token("content", function (req, res) {
   return JSON.stringify(req.body)
 })
+app.use(express.static("build"))
+
 app.use(
   morgan(
     ":method :url :status :res[content-length] - :response-time ms :content"
   )
 )
+
+app.use(cors())
 
 let persons = [
   {
@@ -84,7 +89,7 @@ app.delete("/api/persons/:id", (request, response) => {
   response.status(204).end()
 })
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
